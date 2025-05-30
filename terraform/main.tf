eba0e88246d5aff417d051b56cb0750e1a9d54b3
preyelg <geeftedpl@gmail.com>
2025-05-30 14:43:40 -0500

    initial

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,11 +7,11 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
-      desired_size = 2
-      max_size     = 3
-      min_size     = 1
+      desired_size   = 2
+      max_size       = 3
+      min_size       = 1
       instance_types = ["t3.medium"]
-      key_name = var.key_pair_name
+      key_name       = var.key_pair_name
     }
   }
 
@@ -19,21 +19,21 @@ module "eks" {
 }
 
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  name   = "gitops-vpc"
-  cidr   = "10.0.0.0/16"
+  source  = "terraform-aws-modules/vpc/aws"
+  name    = "gitops-vpc"
+  cidr    = "10.0.0.0/16"
 
   azs             = ["us-east-2a", "us-east-2b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
 }
 
 resource "helm_release" "argocd" {
-  name       = "argocd"
-  namespace  = "argocd"
+  name             = "argocd"
+  namespace        = "argocd"
   create_namespace = true
 
   repository = "https://argoproj.github.io/argo-helm"
@@ -43,4 +43,7 @@ resource "helm_release" "argocd" {
   values = [
     file("${path.module}/argocd-values.yaml")
   ]
+
+  # Ensure EKS is fully ready before deploying Helm
+  depends_on = [module.eks]
 }
